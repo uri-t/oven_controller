fdbae83627f54e6c2bb141c7d1b8f71cb76994e8
uri <utayvah@gmail.com>
2021-09-08 18:57:21 -0400

    add derivative term

--- client.py
+++ client.py
@@ -13,15 +13,31 @@ def write_duty_cycle(on_frac):
 def controller_fn():
 
     dT_sum = 0
+    dT_prev = False
+    t_prev = datetime.now()
+    
     def fn(dT):
         nonlocal dT_sum
+        nonlocal dT_prev
+        nonlocal t_prev
         
         k_p = 0.02
         k_i = 0.001
+        k_d = 0.2
+        
         dT_sum += dT
+
+        dT_prev = dT_prev or dT
+        t_curr = datetime.now()
+        dt = (t_curr - t_prev).seconds/60
+        deriv = (dT - dT_prev)/dt
+
+        t_prev = t_curr
+        dT_prev = dT
         
         print("integral term: %0.2f" % dT_sum)
-        return min(0.5, max(0, k_p*dT + k_i*dT_sum))
+        print("deriv: %0.2f" % deriv)
+        return min(0.5, max(0, k_p*dT + k_i*dT_sum + k_d*deriv))
 
     return fn
 
@@ -30,6 +46,7 @@ f = open('results/%s.txt' % datetime.isoformat(datetime.now(), \
                                                    timespec='minutes'), 'a')
 
 
-run_controller([0, 60, 120], [25, 180, 180], writer_fn(f), \
+run_controller([0, 115, 125, 158, 188], \
+               [25, 370, 370, 470, 470], writer_fn(f), \
                                              write_duty_cycle,
                                              controller_fn())
